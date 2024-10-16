fb019d28fdfe586b800d43ea0acc098ed9d5952b
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-17 08:47:05 +1100

    hamburger

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const age = 20;
 
-if(age>=18){
-    console.log("You can enter the club!");
-} else if(age<15){
-    console.log("Kiddo! Go Home.")
-}else{
-console.log("You cannot enter the club! Sorry!");
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("menu-nav").classList.toggle("change");
 }
